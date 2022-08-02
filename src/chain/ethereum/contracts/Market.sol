feca9bdbec7cde305507e8815330eb5a96cf640a
tmpITK <torok.mark.patrik@gmail.com>
2022-08-02 18:55:02 +0200

    Add test ipfs hash and basic minting and owning logic

---
+++ src/chain/ethereum/contracts/Market.sol
@@ -0,0 +1,57 @@
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Market {
+
+    address [] public nftList;
+    mapping(address => address []) public ownerMap;
+
+    function mint(string memory name, string memory imgHash) public payable returns(address){
+        address newNft = address(new Nft(name, msg.sender, imgHash));
+        nftList.push(newNft);
+        addNftToOwner(newNft, msg.sender);
+
+        return newNft;
+    }
+
+    function addNftToOwner(address nftAddress, address owner) private {
+
+        if(ownerMap[owner].length > 0) {
+            ownerMap[owner].push(nftAddress);
+        }
+        ownerMap[owner] = [nftAddress];
+    }
+
+    function getOwnedNft(address owner, uint index) private view returns (address){
+        require(ownerMap[owner].length > 0);
+        // I will call this only so I will have to chekc for size beforehand
+        return ownerMap[owner][index];
+    }
+
+}
+
+
+contract Nft {
+
+    string name;
+    address owner;
+    string imageHash;
+
+    constructor (string memory assetName, address assetOwner, string memory ipfsHash) {
+        name = assetName;
+        owner = assetOwner;
+        imageHash = ipfsHash;
+    }
+
+    function getOwner() public view  returns (address){
+        return owner;
+    }
+
+    function getName() public view  returns (string memory){
+        return name;
+    }
+
+    function getImageHash() public view  returns (string memory){
+        return imageHash;
+    }
+
+}
