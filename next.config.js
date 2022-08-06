5627b5c2c6679dfd13cf56a99fa53a45639638a9
tmpITK <torok.mark.patrik@gmail.com>
2022-08-06 08:23:34 +0200

    Temp fix fs problem

--- next.config.js
+++ next.config.js
@@ -2,8 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  env: {
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
   }
 }
 
 module.exports = nextConfig
+
