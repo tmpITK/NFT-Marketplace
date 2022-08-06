53b0e28456fc351a28c26c85ead4957d0fbfa3d1
tmpITK <torok.mark.patrik@gmail.com>
2022-08-06 14:14:49 +0200

    Add card group for nft

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Card } from "semantic-ui-react";
 
 import Market from "../src/chain/ethereum/market";
 import Nft from "../src/chain/ethereum/nft";
@@ -9,7 +10,7 @@ class MarketplaceIndex extends Component {
   static async getInitialProps(props) {
     const market = await Market("0xD79aD96386972832232D1E2EB292E20291be1cd4");
     const numberOfNfts = await market.methods.getNumberOfNfts().call();
-    
+
     const nfts = await Promise.all(
       Array(parseInt(numberOfNfts))
         .fill()
@@ -17,7 +18,12 @@ class MarketplaceIndex extends Component {
             const nftAddress = await market.methods.nftList(index).call();
             const nft = Nft(nftAddress);
             const nftInfo = await nft.methods.getNftInfo().call();
-            return nftInfo;
+
+            return {
+              name: nftInfo[0],
+              address: nftInfo[1],
+              url: nftInfo[2],
+            };
         })
     );
     console.log(nfts);
@@ -27,10 +33,29 @@ class MarketplaceIndex extends Component {
     };
   }
 
+  getNumbRandomArrayElements(arr, num) {
+    const shuffled = [...arr].sort(() => 0.5 -Math.random());
+    return shuffled.slice(0, num);
+  }
+
+  renderNftPreview() {
+    const nfts = this.props.nfts;
+    const toShowCase = this.getNumbRandomArrayElements(nfts, 1);
+
+    const cardItems = toShowCase.map((nft) => {
+      return {
+        header: nft.name,
+        description: nft.address,
+        image: nft.url
+      }
+    });
+    return <Card.Group items={cardItems} />;
+  }
+
   render() {
     return(
       <div>          
-      NFT name {this.props.nfts[0][0]} NFT owner {this.props.nfts[0][1]} NFT IPFS url {this.props.nfts[0][2]}
+        {this.renderNftPreview()}
       </div>
       
     );
