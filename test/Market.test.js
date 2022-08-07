28712715041f03e961fdbf65cc556433bd954a62
tmpITK <torok.mark.patrik@gmail.com>
2022-08-07 10:07:29 +0200

    Add mocha and first test

---
+++ test/Market.test.js
@@ -0,0 +1,38 @@
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider({gasLimit: 8000000}));
+
+const compiledMarket = require("../src/chain/ethereum/build/Market.json");
+const compiledNft = require("../src/chain/ethereum/build/Nft.json");
+
+let accounts;
+let market;
+
+beforeEach(async() => {
+    accounts = await web3.eth.getAccounts();
+
+    market = await new web3.eth.Contract(compiledMarket.abi)
+        .deploy({data: compiledMarket.evm.bytecode.object})
+        .send({from: accounts[0], gas: 8000000});
+
+});
+
+describe("Market", () => {
+
+    it("mints an nft successfully", async () => {
+
+        await market.methods.mint("testName", "testHash").send({from: accounts[0], gas:1000000});
+        const numberOfNfts = await market.methods.getNumberOfNfts().call();
+        assert(numberOfNfts == 1);
+
+        const testNftAddress = await market.methods.nftList(0).call();
+        const mintedNft = await new web3.eth.Contract(compiledNft.abi, testNftAddress);
+
+        const nftInfo = await mintedNft.methods.getNftInfo().call();
+
+        assert(nftInfo[0] == "testName");
+        assert(nftInfo[2] == "testHash");
+    });
+
+});
