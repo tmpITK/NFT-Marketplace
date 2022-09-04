d75657e34ca397b0b67a40b450efd5d1eb539aaa
tmpITK <torok.mark.patrik@gmail.com>
2022-09-04 08:55:59 +0200

    Rename paths in dfx json and remove some unused imports

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import NftListRenderingComponent from '../components/NftListRenderingComponent';
 import 'semantic-ui-css/semantic.min.css'
 import ChainAdapter from "../src/chain/adapters/ChainAdapter";
-import { Card } from 'semantic-ui-react';
-
 
 import Layout from "../components/Layout";
 
--- pages/market.js
+++ pages/market.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import ChainAdapter from '../src/chain/adapters/ChainAdapter';
-import { Card } from 'semantic-ui-react';
 import NftListRenderingComponent from '../components/NftListRenderingComponent';
 
 
