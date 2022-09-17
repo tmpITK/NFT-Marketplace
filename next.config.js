06924b10641dea2b74b189f0656ebeaf16dd3ec0
tmpITK <torok.mark.patrik@gmail.com>
2022-09-17 10:13:07 +0200

    Revert "Tinker around with configs"
    This reverts commit 86a4235136fad49465155570377cd6ed045f6e22.

--- next.config.js
+++ next.config.js
@@ -1,30 +1,15 @@
 /** @type {import('next').NextConfig} */
-
-
-const DFXWebPackConfig = require("./dfx.webpack.config")
-DFXWebPackConfig.initCanisterIds()
-
-const webpack = require("webpack")
-
-// Make DFX_NETWORK available to Web Browser with default "local" if DFX_NETWORK is undefined
-const EnvPlugin = new webpack.EnvironmentPlugin({
-  DFX_NETWORK: "local"
-})
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+
+    return config;
+  },
   env : {
      MARKET_ADDRESS: "0x37AdE76f3846F380c0766179a2b6C19a9Bc929C5",
      CHAIN_ENV: process.env.CHAIN_ENV
-  },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Plugin
-    config.plugins.push(EnvPlugin);
-    config.resolve.fallback = { fs: false };
-
-    // Important: return the modified config
-    return config
   }
 }
 
