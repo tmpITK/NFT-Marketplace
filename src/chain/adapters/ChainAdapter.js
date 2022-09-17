8f5f0ac486ad455f2785f2e2a7f62b1a3cfb79f7
tmpITK <torok.mark.patrik@gmail.com>
2022-09-17 10:13:07 +0200

    Revert "[WIP] Make dfinity dynamic only"
    This reverts commit 808206fc8b4165092ac88b2f421f0844bd65d976.

--- src/chain/adapters/ChainAdapter.js
+++ src/chain/adapters/ChainAdapter.js
@@ -1,18 +1,8 @@
-import web3 from "../ethereum/web3";
-import Market from "../ethereum/market";
-import { marketplace } from "../dfinity/declarations/marketplace";
-import EthereumAdapter from "./EthereumAdapter";
+import dynamic from 'next/dynamic'
 
-let ChainAdapter = null;
+const DynamicChainAdapter = dynamic(
+    () => import('./StaticChainAdapter'),
+    { ssr: false }
+)
 
-if (process.env.CHAIN_ENV === 'ethereum') {
-    const market = Market(process.env.MARKET_ADDRESS);
-    ChainAdapter = new EthereumAdapter(web3, market);
-} else if (process.env.CHAIN_ENV === 'dfinity') {
-    console.log("MIVAAAAN")
-    const DfinityAdapter = (await import('./DfinityAdapter')).default
-    ChainAdapter = new DfinityAdapter(marketplace);
-    console.log("LOADEEEED")
-}
-
-export default ChainAdapter;
+export default DynamicChainAdapter;
