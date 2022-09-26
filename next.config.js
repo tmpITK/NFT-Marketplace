ac104b6b4b92b3f2401aea97ab878ed6432265ad
tmpITK <torok.mark.patrik@gmail.com>
2022-09-26 18:24:32 +0200

    Add proxy for backend calls

--- next.config.js
+++ next.config.js
@@ -1,22 +1,24 @@
 /** @type {import('next').NextConfig} */
 
-
 const DFXWebPackConfig = require("./dfx.webpack.config")
-DFXWebPackConfig.initCanisterIds()
-
 const webpack = require("webpack")
 
 // Make DFX_NETWORK available to Web Browser with default "local" if DFX_NETWORK is undefined
 const EnvPlugin = new webpack.EnvironmentPlugin({
   DFX_NETWORK: "local",
 })
-
+const {__CANDID_UI_CANISTER_ID, ASSETS_CANISTER_ID, MARKETPLACE_CANISTER_ID, NFT_CANISTER_ID} = DFXWebPackConfig.initCanisterIds()
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   env : {
-     MARKET_ADDRESS: "0x37AdE76f3846F380c0766179a2b6C19a9Bc929C5",
-     CHAIN_ENV: process.env.CHAIN_ENV
+    
+    MARKET_ADDRESS: "0x37AdE76f3846F380c0766179a2b6C19a9Bc929C5",
+    CHAIN_ENV: process.env.CHAIN_ENV,
+    CANDID_UI_CANISTER_ID: __CANDID_UI_CANISTER_ID,
+    ASSETS_CANISTER_ID: ASSETS_CANISTER_ID,
+    MARKETPLACE_CANISTER_ID: MARKETPLACE_CANISTER_ID,
+    NFT_CANISTER_ID: NFT_CANISTER_ID
   },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Plugin
@@ -26,6 +28,14 @@ const nextConfig = {
 
     // Important: return the modified config
     return config
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/api/:path*' // Proxy to Backend
+      }
+    ]
   }
 }
 
