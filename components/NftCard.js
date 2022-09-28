ab71214351595d1375fe42ad006a1b3249f81bcf
tmpITK <torok.mark.patrik@gmail.com>
2022-09-28 17:41:44 +0200

    Make react functional component async with use effect

--- components/NftCard.js
+++ components/NftCard.js
@@ -1,43 +1,59 @@
 import { Card, Image } from 'semantic-ui-react';
 import { Link } from '../routes';
 import Buy from './Buy';
-import ChainAdapter from '../src/chain/adapters/ChainAdapter';
+import DfinityAdapter from '../src/chain/adapters/DfinityAdapter';
 import React from 'react';
-
+import { useState, useEffect } from 'react';
 
 const NftCard = props => {
-    const {nft, isOwner, isListing} = props;
-    return (
-        <Card style={{background: 'linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5))'}}>
-            <img src={ChainAdapter.getNftImage(nft)} style={{height: "200px", objectFit: "cover", objectPosition: "100% 50%"}}/>
-            <Card.Content >
-                <Card.Header>{nft.name}</Card.Header>
-                <Card.Meta>
-                    <Link route={`/nft/${nft.address}`}>
-                        <a>Details<br/></a>
-                    </Link>
-                </Card.Meta>
-                <Card.Description>
-                    {isOwner && 
-                        <Link route={`/nft/sell/${nft.address}`}>
-                            <a>Sell</a>
-                        </Link>
-                    }
-                    {
-                        isListing &&
-                        <Buy marketAddress={process.env.MARKET_ADDRESS } nftAddress={nft.address}></Buy>
-                    }
-                </Card.Description>
-            </Card.Content>
+    const [nftCard, setNftCard] = useState(null);
+    useEffect(() => {
+        async function createNftCard() {
+            if(typeof props !== "undefined") {
+                console.log("gecc")
+                console.log(props)
+                const {nft, isOwner, isListing} = props;
+                const marketplace = (await import('../src/declarations/marketplace')).marketplace;
+                console.log(marketplace)
+                const chainAdapter = new DfinityAdapter(marketplace);
+                setNftCard(
+                    <Card style={{background: 'linear-gradient(rgba(255,255,255,0.5), rgba(255,255,255,0.5))'}}>
+                        <img src={chainAdapter.getNftImage(nft)} style={{height: "200px", objectFit: "cover", objectPosition: "100% 50%"}}/>
+                        <Card.Content >
+                            <Card.Header>{nft.name}</Card.Header>
+                            <Card.Meta>
+                                <Link route={`/nft/${nft.address}`}>
+                                    <a>Details<br/></a>
+                                </Link>
+                            </Card.Meta>
+                            <Card.Description>
+                                {isOwner && 
+                                    <Link route={`/nft/sell/${nft.address}`}>
+                                        <a>Sell</a>
+                                    </Link>
+                                }
+                                {
+                                    isListing &&
+                                    <Buy marketAddress={process.env.MARKET_ADDRESS } nftAddress={nft.address}></Buy>
+                                }
+                            </Card.Description>
+                        </Card.Content>
+        
+                        <Card.Content extra>
+                            <Link route={`/user/${nft.owner}`}>
+                                <a>Owner</a>
+                            </Link>
+                        </Card.Content>
+        
+                    </Card>
+                )
+            }
+            return <br/>;
+        };
+        createNftCard();
+    }, []);
+    return nftCard;
+}
 
-            <Card.Content extra>
-                <Link route={`/user/${nft.owner}`}>
-                    <a>Owner</a>
-                </Link>
-            </Card.Content>
-
-        </Card>
-    )
-};
 
 export default NftCard;
