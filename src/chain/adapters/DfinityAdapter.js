a92506a72e75b6b59680eb1e8b8602fd3635e29a
tmpITK <torok.mark.patrik@gmail.com>
2022-09-29 17:29:26 +0200

    Make getOwnedNfts actually give back the nfts

--- src/chain/adapters/DfinityAdapter.js
+++ src/chain/adapters/DfinityAdapter.js
@@ -1,10 +1,13 @@
 import { getIpfsUrlFromHash } from "../../utils";
 import { Principal } from "@dfinity/principal";
+import { Actor, HttpAgent } from "@dfinity/agent";
 
 class DfinityAdapter {
 
     constructor(market) {
         this.market = market;
+        const localHost = "http://localhost:8000/"; // TODO env var
+        this.agent = new HttpAgent({ host: localHost });
     }
 
     async mint(name, url) {
@@ -14,7 +17,33 @@ class DfinityAdapter {
     }
 
     async getOwnedNfts(userId) {
-        return await this.market.getOwnedNfts(userId);
+
+        const ownedNftPrincipals = await this.market.getOwnedNfts(Principal.fromText(userId));
+        const { idlFactory } = (await import('../../declarations/nft'));
+
+        const nfts = await Promise.all(
+            Array(parseInt(ownedNftPrincipals.length))
+                .fill()
+                .map(async (element, index) => {
+                    const nftAddress = ownedNftPrincipals[index];
+                    const nft = await this.getNft(nftAddress, idlFactory);;
+                    return {
+                        name: await nft.getName(),
+                        owner: await nft.getOwner(),
+                        ipfsHash: await nft.getIpfsHash(),
+                        address: nftAddress,
+                    };
+                })
+            );
+
+        return nfts;
+    }
+
+    getNft(principal, idlFactory) {
+        return Actor.createActor(idlFactory, {
+            agent: this.agent,
+            canisterId: principal,
+          });
     }
 
     async getNftList() {
@@ -30,6 +59,10 @@ class DfinityAdapter {
     async getListedNfts() {
         console.log("Nfts listed");
         return [];
+    }
+
+    getNftImage(nft) {
+        return getIpfsUrlFromHash(nft.ipfsHash);
     }
 
 }
